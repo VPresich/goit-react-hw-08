91654e5027ad68bde0751caf3f895ebc2658ced8
VPresich <valya.alexandrova.presich@gmail.com>
2024-02-28 19:18:18 +0100

    for test

--- src/jsx/redux/auth/operations.js
+++ src/jsx/redux/auth/operations.js
@@ -85,6 +85,7 @@ export const refreshUser = createAsyncThunk(
       // If there is a token, add it to the HTTP header and perform the request
       setAuthHeader(persistedToken);
       const res = await axios.get('/users/current');
+      console.log('refresh', res.data);
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
